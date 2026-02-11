ad013c673e9404cae3339267e778101ab6ea5050
phat-dat-ag <ag67phatdat@gmail.com>
2026-02-11 15:53:50 +0700

    feat(auth): add GuestHeader and Footer for guest layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,14 @@
+import GuestHeader from "../(guest)/components/GuestHeader";
+import Footer from "../components/Footer";
+
 export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="w-[60%] mx-auto mt-8">{children}</div>;
+  return (
+    <div>
+      <GuestHeader />
+      <div className="w-[60%] mx-auto mt-8">{children}</div>
+      <Footer />
+    </div>
+  );
 }
